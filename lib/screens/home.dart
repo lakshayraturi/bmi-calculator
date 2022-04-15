6b30efeec4b5a5a87058e45a16fca4252ffcb6cb
Lakshay Raturi <85173668+lakshayraturi@users.noreply.github.com>
2022-04-15 22:10:10 +0530

    init

---
+++ lib/screens/home.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import '../constants/app_constants.dart';
+import '../widgets/left_bar.dart';
+import '../widgets/right_bar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+  double _bmiResult = 0;
+  String _textResult = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI Calculator",
+          style: TextStyle(
+            color: accentHexColor,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 130,
+                child: TextField(
+                  controller: _heightController,
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.w300,
+                    color: accentHexColor,
+                  ),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Height",
+                    hintStyle: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white.withOpacity(0.8),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 130,
+                child: TextField(
+                  controller: _weightController,
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.w300,
+                    color: accentHexColor,
+                  ),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Weight",
+                    hintStyle: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white.withOpacity(0.8),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onTap: () {
+              double _h = double.parse(_heightController.text);
+              double _w = double.parse(_weightController.text);
+              setState(() {
+                _bmiResult = _w / (_h * _h);
+                if (_bmiResult > 25) {
+                  _textResult = "You're over weight";
+                } else if (_bmiResult >= 18.5 && _bmiResult <= 25) {
+                  _textResult = "You have normal weight";
+                } else {
+                  _textResult = "You're under weight";
+                }
+              });
+            },
+            child: Container(
+              child: Text(
+                "Calculate",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: accentHexColor,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Container(
+            child: Text(
+              _bmiResult.toStringAsFixed(2),
+              style: TextStyle(
+                fontSize: 90,
+                color: accentHexColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Visibility(
+            visible: _textResult.isNotEmpty,
+            child: Container(
+              child: Text(
+                _textResult,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                  color: accentHexColor,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          LeftBar(
+            barWidth: 40,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          LeftBar(
+            barWidth: 70,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          LeftBar(
+            barWidth: 40,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          RightBar(
+            barWidth: 70,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          RightBar(
+            barWidth: 70,
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/widgets/left_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../constants/app_constants.dart';
+
+class LeftBar extends StatelessWidget {
+  final double barWidth;
+
+  const LeftBar({Key? key, required this.barWidth}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+            color: accentHexColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
